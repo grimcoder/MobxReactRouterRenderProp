4e3c1638fdb4cba4807dcc6d6738c90d9fa81b28
taras kovtun <taraskovtun@tarass-MacBook-Pro.local>
2018-10-08 21:45:43 -0700

    some change

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,12 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import {observable} from 'mobx';
-import {observer} from 'mobx-react';
+import {inject, observer} from 'mobx-react';
 import DevTools from 'mobx-react-devtools';
+import { Provider } from 'mobx-react';
+import createBrowserHistory from 'history/createBrowserHistory';
+import { RouterStore, syncHistoryWithStore } from 'mobx-react-router';
+import { Router, Route } from 'react-router';
 
 class AppState {
 
@@ -24,25 +28,36 @@ class AppState {
     }
 }
 
+// @inject('routing')
 @observer
 class App extends React.Component<{appState: AppState}, {}> {
     componentDidMount(){
         setTimeout(()=>{
           this.props.appState.updateLoading(false)
+          this.forceUpdate();
         }, 5000)
         
       }
 
     render() {
         return (
-
             <div>
-            <HOC isLoading={this.props.appState.loading}>
-            {         (isLoading)=>isLoading ? <span>Loading</span> : <span>Ready</span>  }
-            </HOC> 
-            <button onClick={this.onReset}>Reset counter</button>
-            <div>Seconds passed:  {this.props.appState.counter}</div>
-          </div>
+            
+            <Route {...this.props} to='/' children={({...props})=>{
+                console.log('drawn')
+                return (                
+                <div>
+                <HOC isLoading={this.props.appState.loading}>
+                    {
+                        (isLoading)=>isLoading ? <span>Loading</span> : <span>Ready</span>  
+                    }
+                </HOC>
+                <button onClick={this.onReset}>Reset counter</button>
+                <div>Seconds passed:  {this.props.appState.counter}</div>
+              </div>
+             )}}></Route>
+
+         }</div>
         );
      }
 
@@ -57,5 +72,22 @@ const HOC = (props) => {
 }
 
 const appState = new AppState();
+const browserHistory = createBrowserHistory();
+const routingStore = new RouterStore();
 
-ReactDOM.render(<App appState={appState} />, document.getElementById('root'));
+const stores = {
+  // Key can be whatever you want
+  routing: routingStore,
+  // ...other stores
+};
+
+const history = syncHistoryWithStore(browserHistory, routingStore);
+  
+const WrappedApp = ((props)=>
+    <Provider {...stores}>
+        <Router history={history}>
+            <App {...props} />
+        </Router>
+    </Provider>)
+
+ReactDOM.render(<WrappedApp appState={appState} />, document.getElementById('root'));
